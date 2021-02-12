e7038fd9499b96777470c10ea297499b28584c8f
Patel1198 <65984709+Patel1198@users.noreply.github.com>
2021-02-12 02:20:45 -0500

    Add files via upload

---
+++ lec92_securityRegistrationAndHTTP/src/main/java/ca/sheridancollege/sharamik/lec92_securityRegistrationAndHTTPS.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.sharamik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lec92_securityRegistrationAndHTTPS {
+
+	public static void main(String[] args) {
+		SpringApplication.run(lec92_securityRegistrationAndHTTPS.class, args);
+	}
+
+}
