c0fab19fbe5415a1e3b9e7a0f8a604044b03f40d
Patel1198 <65984709+Patel1198@users.noreply.github.com>
2021-02-12 02:10:22 -0500

    Add files via upload

---
+++ ExerciseW8_2/src/main/java/ca/sheridancollege/pate2516/ExerciseW82Application.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.pate2516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseW82Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseW82Application.class, args);
+	}
+
+}
