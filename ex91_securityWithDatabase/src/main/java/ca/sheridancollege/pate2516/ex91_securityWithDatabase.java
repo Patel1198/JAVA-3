7f1a7f64f6db82141863696d5ded5dd25e65a285
Patel1198 <65984709+Patel1198@users.noreply.github.com>
2021-02-12 00:37:01 -0500

    Add files via upload

---
+++ ex91_securityWithDatabase/src/main/java/ca/sheridancollege/pate2516/ex91_securityWithDatabase.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.pate2516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ex91_securityWithDatabase {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ex91_securityWithDatabase.class, args);
+	}
+
+}
